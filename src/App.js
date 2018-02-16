8723b23f5098c56cb62fc8c4776dadb6f59bc84f
gjkthx <gjkthx@gmail.com>
2018-02-16 16:16:19 -0700

    just some css to pass off

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+      <div className="header">
+        <div id="pic"></div>
+        <div className="headerText">Hello</div>
+      </div>
+        <p> is this working?</p>
+        <input></input>
+        <button>this is a button!!</button>
+        <div className="thisBox" >
+          <div className='test'> so so so so so so so so so so so so so so much text</div>
+        </div>
+      </div>
+      
+    );
+  }
+}
+
+export default App;
